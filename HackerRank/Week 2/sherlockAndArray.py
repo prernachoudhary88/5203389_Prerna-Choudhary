40ec7b89def8d2af1abff9a09d239fffd4e8061b
PRERNA CHOUDHARY <prernainspiration88@gmail.com>
2025-08-13 20:23:45 +0530

    Create sherlockAndArray.py

---
+++ HackerRank/Week 2/sherlockAndArray.py
@@ -0,0 +1,14 @@
+def balancedSums(arr):
+    total_sum = sum(arr)
+    left_sum = 0
+    
+    for num in arr:
+        
+        right_sum = total_sum - num - left_sum
+        
+        if left_sum == right_sum:
+            return "YES"
+        
+        left_sum += num
+    
+    return "NO"
